4b5274974f8e4f0167b0a87dafb85cf7f264039b
Confrontend <hamed.fatehi.66@gmail.com>
2022-06-18 18:24:29 +0200

    t

--- packages/blog/src/pages/blog.tsx
+++ packages/blog/src/pages/blog.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BlogTeaser from "../components/post-teaser/post-teaser";
 /**
- * Blog Page 
+ * Blog Page
  */
 export default function Blog() {
   return <BlogTeaser />;
