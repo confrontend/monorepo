353c09a7fb90ff50fb927c9dc421e4bf5223a581
Confrontend <hamed.fatehi.66@gmail.com>
2022-06-18 11:40:14 +0200

    test

--- packages/react-usa-data-charts/src/components/UI/button/ui-button.tsx
+++ packages/react-usa-data-charts/src/components/UI/button/ui-button.tsx
@@ -5,7 +5,9 @@ type UiButtonProps = {
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
   children: ReactNode;
 };
-
+/**
+ * UI Button
+ */
 export default function UiButton({ children, onClick }: UiButtonProps) {
   return <S.ButtonWrapper onClick={onClick}>{children}</S.ButtonWrapper>;
 }
