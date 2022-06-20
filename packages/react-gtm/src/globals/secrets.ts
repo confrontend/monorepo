66b7e208fbcd3dd880d49c112ee0210b16257c14
Confrontend <hamed.fatehi.66@gmail.com>
2022-06-20 13:31:10 +0200

    removing secrets

--- packages/react-gtm/src/globals/secrets.ts
+++ packages/react-gtm/src/globals/secrets.ts
@@ -4,6 +4,6 @@
  * newsapi api key
  * https://newsapi.org/docs/get-started
  */
-export const newsApiKey = "176789073a524a9988f7bfed0f5dfc5b";
+export const newsApiKey = "";
 
-export const firebaseApiKey = "AIzaSyAuSZXh3zmh4Q_feVnuU0wcteMQQ-fykHY";
+export const firebaseApiKey = "";
--- packages/react-usa-data-charts/src/apis/secrets.ts
+++ packages/react-usa-data-charts/src/apis/secrets.ts
@@ -1,1 +1,2 @@
-export const weatherApiKey = "9fd83e60bc28439c925111058221505";
+// TODO remove from code base and move to server.
+export const weatherApiKey = "";
