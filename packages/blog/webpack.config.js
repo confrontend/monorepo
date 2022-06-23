472fd3b5132ae4c60729a55592fa66160676cb78
Confrontend <hamed.fatehi.66@gmail.com>
2022-06-23 18:28:14 +0200

    Webpack shared config

--- packages/blog/webpack.config.js
+++ packages/blog/webpack.config.js
@@ -1,50 +1,15 @@
+const BaseWebpackConfig = require("../../webpack.config.base.js");
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  ...BaseWebpackConfig,
   output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
-  mode: process.env.NODE_ENV || "development",
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  devServer: {
-    // Enable hot reloading
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"],
-      },
-      {
-        test: /\.(css|scss)$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        exclude: /node_modules/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(html)$/,
-        use: "html-loader",
-      },
-    ],
-  },
   plugins: [
+    ...BaseWebpackConfig.plugins,
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
     }),
-    new CleanWebpackPlugin(),
   ],
 };
---
+++ webpack.config.base.js
@@ -0,0 +1,41 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const BaseWebpackConfig = {
+  entry: "./src/index.tsx",
+
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    // Enable hot reloading
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ["url-loader", "style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        exclude: /node_modules/,
+        use: ["url-loader"],
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
+
+module.exports = BaseWebpackConfig;
